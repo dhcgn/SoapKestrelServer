4b533fbad4b299d8c9c38a2ae4709ec79caa3623
Daniel <dhcgn@secure.mailbox.org>
2018-03-06 21:26:34 +0100

    Clean up

--- SoapCoreServer/Startup.cs
+++ SoapCoreServer/Startup.cs
@@ -1,33 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.ServiceModel;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SoapCore;
 
 namespace SoapCoreServer
 {
+    public class ServiceContractImpl : IServiceContract
+    {
+        public string Ping()
+        {
+            return DateTime.UtcNow.ToString("O");
+        }
+    }
+
+    [ServiceContract]
+    public interface IServiceContract
+    {
+        [OperationContract]
+        string Ping();
+    }
+
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.TryAddSingleton<ServiceContractImpl>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseSoapEndpoint<ServiceContractImpl>("/Service.asmx", new BasicHttpBinding());
 
-            app.Run(async (context) =>
+            app.Run(context =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                context.Response.Redirect("Service.asmx");
+                return Task.Run(() => { });
             });
         }
     }
